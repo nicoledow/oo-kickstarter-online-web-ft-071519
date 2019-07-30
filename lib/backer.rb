a16059f1326532ccb1a10c6d4da2021d2a03d428
Nicole Dow <nikki.dow71@gmail.com>
2019-07-30 13:38:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,9 +2,12 @@ class Backer
   attr_reader :name
   attr_accessor :backed_projects
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @backed_projects = []
+    @@all << self
   end
   
   def back_project(project)
